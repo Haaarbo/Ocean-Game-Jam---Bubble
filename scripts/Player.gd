86186707d006b522d7139d76bfaa3f5eae948939
JorgeHenriqueBS <106774238+JorgeHenriqueBS@users.noreply.github.com>
2025-01-23 16:23:55 -0400

    Alinhamento de Versão
    Mudanças em andamento no presonagem e na bullet, subindo agora a fim de tentar alinhar as versões.

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -2,13 +2,18 @@ extends CharacterBody2D
 
 
 const SPEED = 430.0
+var collision = get_slide_collision(0)
+
+var life: int = 6
+var grazebar: int = 100
 
 func movement():
 	var direction := Input.get_vector("Left", "Right","Up","Down")
 	velocity = direction*SPEED
 	
-	if Input.is_action_pressed("Shift"):
+	if grazebar>0:
 		velocity = (direction*SPEED)/2
+		grazebar -= 0.005
 
 func _physics_process(delta: float) -> void:
 
@@ -16,3 +21,7 @@ func _physics_process(delta: float) -> void:
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	movement()
 	move_and_slide()
+	if move_and_slide():
+		print(collision.get_collider())
+	
+#func _process(delta: float) -> void:
